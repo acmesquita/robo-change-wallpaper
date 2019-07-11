940c0218c18ea80dc85318925c2149b756a3e7a0
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-07-11 15:42:24 -0300

    Criando robo que busca imagens no google

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const readline = require('readline-sync')
 const robots = {
     input: require('./robots/input'),
     text: require('./robots/text'),
-    state: require('./robots/state')
+    state: require('./robots/state'),
+    image: require('./robots/image')
 }
 
 async function start(){
     
-    robots.input()
-    await robots.text()
+    // robots.input()
+    // await robots.text()
+    await robots.image()
 
     const content = robots.state.load()
     console.dir(content, { depth: null })
---
+++ robots/image.js
@@ -0,0 +1,42 @@
+const google = require('googleapis').google
+const customSearch = google.customsearch('v1')
+const state = require('./state')
+
+const googleSearchCredentials = require('../credentials/google-search.json')
+
+async function robot(){
+    const content = state.load()
+
+    await fetchImagesOfAllSentences(content)
+
+    state.save(content)
+    
+    async function fetchImagesOfAllSentences(content){
+        for (const sentence of content.sentences) {
+            const query = `${content.searchTerm} ${sentence.keywords[0]}`
+            sentence.images = await fetchGoogleAndReturnImagesLinks(query)
+
+            sentence.googleSearchQuery =  query
+        }
+    }
+    
+    async function fetchGoogleAndReturnImagesLinks(query){
+        const response = await customSearch.cse.list({
+            key: googleSearchCredentials.apiKey,
+            cx: googleSearchCredentials.searchEngineId,
+            q: query,
+            searchType: 'image',
+            imgSize: 'xlarge',
+            num: 2
+        })
+
+        const imagesUrl = response.data.items.map((item) => {
+            return item.link
+        })
+
+        return imagesUrl;
+    }
+
+}
+
+module.exports= robot
